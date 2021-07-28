d5310f7bbaf0ffc2aeeaf14839e0ba6c2f7c333b
kabirsingh04 <kabirsingh0405@gmail.com>
2021-07-28 23:13:06 +0530

    first commit

---
+++ Intro-To-OOPS/StrudentUse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StrudentUse {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Student s1 = new Student();
+
+        s1.Name = "Ankush";
+        s1.RollNo = 001;
+
+        Student s2 = new Student();
+        s2.Name = "Amit";
+        s2.RollNo = 002;
+        System.out.println(s1.Name + " " + s1.RollNo);
+        System.out.println(s2.Name + " " + s2.RollNo);
+        // System.out.println(s1);
+    }
+}
---
+++ Intro-To-OOPS/Student.java
@@ -0,0 +1,4 @@
+public class Student {
+    int RollNo;
+    String Name;
+}
